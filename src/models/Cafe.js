c8465788c503cf1b7b1469d3d5def29c179434aa
yrkeviin <kevineziquiellima160@gmail.com>
2025-02-11 10:29:52 -0300

    Feat: Create class cafe

---
+++ src/models/Cafe.js
@@ -0,0 +1,10 @@
+class Cafe {
+    constructor(id, pedido, cliente, valor) {
+        this.id = id;
+        this.pedido = pedido;
+        this.cliente = cliente;
+        this.valor = valor;
+    }
+}
+
+module.exports = Cafe;
