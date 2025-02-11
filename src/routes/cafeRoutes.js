72e663722de3c5df455a0a64a76c6e8077f85b4d
yrkeviin <kevineziquiellima160@gmail.com>
2025-02-11 10:49:11 -0300

    Feat: Create routes

---
+++ src/routes/cafeRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const cafeController = require("../controllers/cafeController");
+
+router.get("/menu", cafeController.listarCafes);
+router.post("/order", cafeController.adicionarCafe);
+router.get("/order/:id", cafeController.buscarCafePorId);
+router.delete("/order/:id", cafeController.deletarCafe);
