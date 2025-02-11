e56d7fcc0ff92b40ccc8ef1b7f75f46f4f94f958
yrkeviin <kevineziquiellima160@gmail.com>
2025-02-11 10:36:51 -0300

    Feat: Add methods and create listcafe

---
+++ src/models/ListCafe.js
@@ -0,0 +1,19 @@
+const Cafe = require("./Cafe");
+
+class ListCafe {
+    constructor() {
+        this.cafes = [];
+        this.proximoId = 1;
+    }
+    addCafe(pedido, cliente, valor) {
+        const newCafe = new newCafe(this.proximoId++, pedido, cliente, valor);
+        this.cafes.push(newCafe);
+        return newCafe;
+    }
+    totalCafes() {
+        return this.cafes;
+    }
+    buscarCafePorId(id) {
+        return this.cafes.find(cafe => cafe.id === id);
+    }
+}
